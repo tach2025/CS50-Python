00a46dc310fe777a554bb70b573fa7d7cdce0661
Tejas Acharya <193575335+tach2025@users.noreply.github.com>
2025-01-03 16:47:53 +0530

    Add einstein

---
+++ pset-0/einstein/einstein.py
@@ -0,0 +1,6 @@
+# Prompts the user for mass(in kg)
+m = int(input("m: "))
+# Speed of light(in meters per second)
+c = 300000000
+# Print the Energy using Einstein's equation
+print(f"E: {m * c * c}")
